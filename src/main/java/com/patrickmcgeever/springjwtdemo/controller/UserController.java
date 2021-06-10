a177d1b10192e455d9a10875ab003c419ea596f0
Patrick McGeever <p.s.mcgeever@gmail.com>
2021-06-10 18:39:50 +0100

    Reorganized into packages.

--- src/main/java/com/patrickmcgeever/springjwtdemo/controller/UserController.java
+++ src/main/java/com/patrickmcgeever/springjwtdemo/controller/UserController.java
@@ -1,23 +1,22 @@
-package com.patrickmcgeever.springjwtdemo;
+package com.patrickmcgeever.springjwtdemo.controller;
 
 import com.patrickmcgeever.springjwtdemo.domain.User;
+import com.patrickmcgeever.springjwtdemo.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/user")
 @AllArgsConstructor
 public class UserController {
-    private UserService userService;
+    private @Autowired UserService userService;
 
     @PostMapping()
     public ResponseEntity<String> saveUser(@RequestBody User user) {
         userService.saveDto(user);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
--- src/main/java/com/patrickmcgeever/springjwtdemo/service/UserService.java
+++ src/main/java/com/patrickmcgeever/springjwtdemo/service/UserService.java
@@ -1,15 +1,18 @@
-package com.patrickmcgeever.springjwtdemo;
+package com.patrickmcgeever.springjwtdemo.service;
 
 import com.patrickmcgeever.springjwtdemo.dao.HibernateDAO;
 import com.patrickmcgeever.springjwtdemo.domain.User;
 import com.sun.istack.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private @Autowired BCryptPasswordEncoder bCryptPasswordEncoder;
     private @Autowired HibernateDAO<User> userDAO;
@@ -18,5 +21,10 @@ public class UserService {
     public void saveDto(@NotNull User user) {
         User newUser = user.withPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userDAO.persist(newUser);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        throw new UnsupportedOperationException("Loading a user is not implemented yet!");
     }
 }
